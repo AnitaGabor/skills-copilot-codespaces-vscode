a17c806f31f11383a5a23c70f38b142838b8cc12
AnitaGabor <79316759+AnitaGabor@users.noreply.github.com>
2025-02-19 12:54:53 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+  // Create a new object
+  var member = new Object();
+
+  // Add some properties
+  member.name = "John";
+  member.age = 30;
+  member.skills = ["JavaScript", "HTML", "CSS"];
+
+  // Return the object
+  return member;
+}
